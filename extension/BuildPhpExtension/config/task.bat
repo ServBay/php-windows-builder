93020fc1efed226f3351b010091fe69e493860cb
A 38D Bra <hongsheng@gmail.com>
2026-04-28 02:42:35 +0800

    fix: 仅当 brotli 库存在时才链接 brotli/zstd（修复非 libcurl 扩展构建失败）
    上一次提交 b1c2149 无条件给所有扩展的 LDFLAGS 添加了
    brotlidec.lib/brotlicommon.lib/libzstd.lib，但 brotli/libzstd
    依赖只在检测到扩展使用 libcurl 时才会被下载到 ..\deps\lib。
    
    结果除 oauth 之外，igbinary/xdebug/imagick/mongodb/phalcon/
    memcached/imap/yaml/apcu/sqlsrv/pdo_sqlsrv 等所有扩展全部
    LNK1181: cannot open input file 'brotlidec.lib'。
    
    改为先 if exist 检测 brotlidec.lib 再设置 LDFLAGS：libcurl
    扩展依旧能链接到 brotli/zstd，其他扩展回到原行为。

--- extension/BuildPhpExtension/config/task.bat
+++ extension/BuildPhpExtension/config/task.bat
@@ -1,5 +1,5 @@
 
-set LDFLAGS=brotlidec.lib brotlicommon.lib libzstd.lib 2>&1
+if exist "..\deps\lib\brotlidec.lib" set LDFLAGS=brotlidec.lib brotlicommon.lib libzstd.lib
 call phpize 2>&1
 call configure --with-php-build="..\deps" OPTIONS --with-mp="disable" 2>&1
 nmake /nologo 2>&1
